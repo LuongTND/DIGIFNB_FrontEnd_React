c81acf910aa7ac632a574d397e04fc8cd32ded54
LuongTND <ducluongtran07@gmail.com>
2024-11-12 14:42:59 +0700

    add api data grab

--- src/api/order_history.api.jsx
+++ src/api/order_history.api.jsx
@@ -1,15 +1,32 @@
 
 
+// import axios from 'axios';
+
+// export const getOrderhistoryApi = async () => {
+//   try {
+//     // const response = await axios.get('http://digifnbv1.runasp.net/api/DataGrab/get-data-oderhistory');
+//     const response = await axios.get('/api/DataGrab/get-data-oderhistory');
+//     return response.data;  // return dữ liệu từ API
+//   } catch (error) {
+//     // console.error('Error fetching order history data:', error);
+//     throw error;
+//   }
+// };
+//const response = await axios.get('https://localhost:7182/api/DataGrab/get-data-oderhistory');
+
 import axios from 'axios';
 
 export const getOrderhistoryApi = async () => {
   try {
-    // const response = await axios.get('http://digifnbv1.runasp.net/api/DataGrab/get-data-oderhistory');
-    const response = await axios.get('/api/DataGrab/get-data-oderhistory');
-    return response.data;  // return dữ liệu từ API
+    const response = await axios.get('/api/DataGrab/get-data-oderhistory', {
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+      },
+    });
+    return response.data;
   } catch (error) {
-    // console.error('Error fetching order history data:', error);
     throw error;
   }
 };
-//const response = await axios.get('https://localhost:7182/api/DataGrab/get-data-oderhistory');
