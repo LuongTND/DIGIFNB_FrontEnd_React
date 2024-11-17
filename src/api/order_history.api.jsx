2980fc893a23d7890b2c1f24babf7a3cf72e692f
LuongTND <ducluongtran07@gmail.com>
2024-11-17 09:34:10 +0700

    add api data grab

--- src/api/order_history.api.jsx
+++ src/api/order_history.api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getOrderhistoryApi = async () => {
   try {
     // const response = await axios.get('http://digifnbv1.runasp.net/api/DataGrab/get-data-oderhistory');
-    const response = await axios.get('/api/DataGrab/get-data-oderhistory');
+    const response = await axios.get('/DataGrab/get-data-oderhistory');
     return response.data;  // return dữ liệu từ API
   } catch (error) {
     // console.error('Error fetching order history data:', error);
